75844ba312e3a7626e8d4ff6f8c7bc371dd74a29
Varun Nischal <nvarun14@gmail.com>
2021-02-20 02:37:16 +0530

    Golang Tutorials

---
+++ hello/hello.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  "fmt"
+  "rsc.io/quote"
+  "example.com/greetings"
+  "log"
+)
+
+func main() {
+
+  fmt.Println("Avengers Assemble!")
+  fmt.Println()
+  
+  fmt.Println(quote.Go())
+  fmt.Println()
+  
+  fmt.Println(greetings.Hello("Steve"))
+  fmt.Println()
+
+  log.SetPrefix("greetings: ")
+  log.SetFlags(0)
+
+  //message, err := greetings.Hello("")
+
+  //if err != nil {
+  //  log.Fatal(err)
+  //}
+
+  //fmt.Println(message)
+  //fmt.Println()
+
+  names := []string{"Tony", "Steve", "Thor"}
+
+  messages, err := greetings.Hellos(names)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(messages)
+  fmt.Println()
+
+}
